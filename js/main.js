8a79c85fdb4f33bf95c8ed343f71b1be4ac18759
FrancyG8 <55449605+FrancyG8@users.noreply.github.com>
2020-01-20 22:06:18 +0100

    Modifiche Fra

--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
-/* $(document).ready(function() {
+$(document).ready(function() {
 
-//giustosbagliato
-  $('#Q1').click(function() {
-  $("#QUESTION6").css({opacity: 0.5
-  });
-  });
+// //giustosbagliato
+//   $('#OP1').click(function() {
+//     $('#q6').css({
+//       fill: "#FFFFFF"
+//     });
+//   });
+//   $('#OP2').click(function() {
+//     $('#q6').css({
+//       fill: "#000000"
+//     });
+//   });
 
-  $('#next').click(function() {
-  location.href='q2.html';
 
-  });
+//--Barra del tempo movimento
+  var tempo = gsap.timeline();
+  tempo.to(".rettangolo-rosso", {duration: 30, scaleY: 0, transformOrigin: "bottom", backgroundColor: "#FF0000"});
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 15, opacity: 0}, "<"); //1
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //2
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //3
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //4
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //5
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //6
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //7
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //8
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //9
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //10
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //11
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //12
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //13
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //14
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //15
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //16
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //17
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //18
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //19
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //20
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //21
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //22
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //23
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //24
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //25
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //26
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //27
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //28
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //29
+  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //30
+});
 
-});*/
 
+//--Prova Azione Slider
 const $img2 = document.querySelector('.image2');
 const $logo = document.querySelector('.transition__logo');
 const $frameBlack = document.querySelector('.page-transition__black');
@@ -27,8 +64,6 @@ let tltransition = new TimelineMax({paused:true})
   .set($img2, {autoAlpha:0})
   .to($frameBlack , 2.2, {scaleX: 0, transformOrigin:'right', ease: Power4.easeInOut})
   .to($logo , .2, {autoAlpha:0 },'-=1.2')
-
-
 
 $button.addEventListener('click', () => {
   tltransition.play(0);
---
+++ js/sketch.js
@@ -0,0 +1,16 @@
+function preload() {}
+
+let rectY = 0;
+let ellY = 725;
+let fr = 60;
+let timer = 00;
+
+
+function setup() {
+  frameRate(fr);
+}
+
+
+function draw() {
+
+}
