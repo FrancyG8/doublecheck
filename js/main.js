3c9e991cc12bf2465d77adb7ea796f74a6ff2161
andreaefebresm <43403041+andreaefebresm@users.noreply.github.com>
2020-01-20 16:31:11 +0100

    factsfino a 3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
 
 //giustosbagliato
-  $('#OP1').click(function() {
-    $('#q6').css({
-      fill: "#FFFFFF"
-    });
-     $("#right").animate({right: '250px'});
+  $('#Q1').click(function() {
+  $("#QUESTION6").css({opacity: 0.5
   });
-  $('#OP2').click(function() {
-    $('#q6').css({
-      fill: "#000000"
-    });
   });
 
+  $('#next').click(function() {
+  location.href='q2.html';
+
+  });
+
+});*/
+
+const $img2 = document.querySelector('.image2');
+const $logo = document.querySelector('.transition__logo');
+const $frameBlack = document.querySelector('.page-transition__black');
+const $frameRed = document.querySelector('.page-transition__red');
+const $button = document.querySelector('#button');
+
+let tltransition = new TimelineMax({paused:true})
+  .fromTo($frameRed , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},)
+  .fromTo($frameBlack , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},.2)
+  .fromTo($logo , 1.6, {xPercent: -100, autoAlpha:0 },{xPercent: 0, autoAlpha:1, ease: Power4.easeInOut},.7)
+  .set($frameRed, {scaleX:0})
+  .set($img2, {autoAlpha:0})
+  .to($frameBlack , 2.2, {scaleX: 0, transformOrigin:'right', ease: Power4.easeInOut})
+  .to($logo , .2, {autoAlpha:0 },'-=1.2')
+
+
+
+$button.addEventListener('click', () => {
+  tltransition.play(0);
 });
