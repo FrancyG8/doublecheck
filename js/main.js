372c690a5a5d001eb1ada1265907dc667b7591d6
FrancyG8 <55449605+FrancyG8@users.noreply.github.com>
2020-01-21 15:25:51 +0100

    Modifiche Fra

--- js/main.js
+++ js/main.js
@@ -46,25 +46,11 @@ $(document).ready(function() {
   tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //28
   tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //29
   tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //30
-});
 
 
-//--Prova Azione Slider
-const $img2 = document.querySelector('.image2');
-const $logo = document.querySelector('.transition__logo');
-const $frameBlack = document.querySelector('.page-transition__black');
-const $frameRed = document.querySelector('.page-transition__red');
-const $button = document.querySelector('#button');
+//--Transizione
+window.addEventListener("beforeunload", function () {
+    document.body.classList.add("animate-out");
+  });
 
-let tltransition = new TimelineMax({paused:true})
-  .fromTo($frameRed , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},)
-  .fromTo($frameBlack , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},.2)
-  .fromTo($logo , 1.6, {xPercent: -100, autoAlpha:0 },{xPercent: 0, autoAlpha:1, ease: Power4.easeInOut},.7)
-  .set($frameRed, {scaleX:0})
-  .set($img2, {autoAlpha:0})
-  .to($frameBlack , 2.2, {scaleX: 0, transformOrigin:'right', ease: Power4.easeInOut})
-  .to($logo , .2, {autoAlpha:0 },'-=1.2')
-
-$button.addEventListener('click', () => {
-  tltransition.play(0);
 });
