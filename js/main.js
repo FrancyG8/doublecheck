d2ef9e646fcdbfafb49e451f1aef97a626e135fa
andreaefebresm <43403041+andreaefebresm@users.noreply.github.com>
2020-01-21 16:40:09 +0100

    q1 svg pronto

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-
-// //giustosbagliato
-//   $('#OP1').click(function() {
-//     $('#q6').css({
-//       fill: "#FFFFFF"
-//     });
-//   });
-//   $('#OP2').click(function() {
-//     $('#q6').css({
-//       fill: "#000000"
-//     });
-//   });
-
+//home
+  var currentX = '';
+  var currentY = '';
+  var movementConstant = .015;
+  $(document).mousemove(function(e) {
+    if(currentX == '') currentX = e.pageX;
+    var xdiff = e.pageX - currentX;
+    currentX = e.pageX;
+     if(currentY == '') currentY = e.pageY;
+    var ydiff = e.pageY - currentY;
+    currentY = e.pageY;
+    $('.immagini').each(function(i, el) {
+        var movement = (i + 2) * (xdiff * movementConstant);
+  	  var movementy = (i + 2) * (ydiff * movementConstant);
+        var newX = $(el).position().left + movement;
+  	  var newY = $(el).position().top + movementy;
+        $(el).css('left', newX + 'px');
+  	  $(el).css('top', newY + 'px');
+    });
+  });
 
 //--Barra del tempo movimento
   var tempo = gsap.timeline();
